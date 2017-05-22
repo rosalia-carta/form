7ddb17387e2abe4cf1b532be692ed7606a28f64c
rosalia-carta <rosalia.carta@gmail.com>
2017-05-22 13:22:13 +0200

    Add files via upload

---
+++ form/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <title>Yoga per tutti</title>
+  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="jumbotron">
+    <div class="container">    
+     <!-- navbar-->
+     <?php include ("fragments/nav.html")?>
+     <!-- welcome-->
+     <?php include ("fragments/welcome.html")?>
+   </div>
+ </div>
+ <!--form contatti -->
+ <?php include ("fragments/form.html")?>
+ <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+ <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+ <!--script type="text/javascript" src="js/fixtures.js"--><!--/script-->
+ <script type="text/javascript" src="vendors/main.js"></script>
+</body>
+</html>
+
+
